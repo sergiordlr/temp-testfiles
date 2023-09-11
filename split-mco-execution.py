402aa2889a563abd51ed41e61fd6910cb1633a18
Sergio Regidor <sregidor@redhat.com>
2023-09-11 09:49:41 +0000

    Allow using stdin in the split mco executions script

--- split-mco-execution.py
+++ split-mco-execution.py
@@ -1,31 +1,31 @@
 import argparse
 import re
+import sys
 
 parser = argparse.ArgumentParser("simple_example")
 parser.add_argument("-e", "--each", help="The number of test case used to split the MCO test cases.", type=int, default=10)
-parser.add_argument("file",nargs=1, help="A file  with a list of all MCO test cases, one case per line")
+parser.add_argument("file", nargs='?', type=argparse.FileType('r'), default=sys.stdin)
 args = parser.parse_args()
 
 num_tests = args.each
-file_path = args.file[0]
+file = args.file
 
 print("Splitting MCO execution to launch {0} tests in each execution", num_tests)
 
 print("{0}", args)
 
-# Using readlines()
-with open(file_path, 'r') as f:
-    count = 0
-    tests_batch = []
-    # Strips the newline character
-    for line in f.readlines():
-        count+=1
-        result = re.search(r".+-(\d+)-.+", line)
-        tc = result.group(1)
-        tests_batch.append(tc)
-        if len(tests_batch) == num_tests:
-            print("|".join(tests_batch))
-            tests_batch = []
+count = 0
+tests_batch = []
+# Strips the newline character
+for line in file.readlines():
+    count+=1
+    result = re.search(r".+-(\d+)-.+", line)
+    tc = result.group(1)
+    tests_batch.append(tc)
+    if len(tests_batch) == num_tests:
+        print("|".join(tests_batch))
+        tests_batch = []
 
-    print("|".join(tests_batch))
-    print("Total: {0} test cases".format(count))
+print("|".join(tests_batch))
+print("Total: {0} test cases".format(count))
+file.close()
